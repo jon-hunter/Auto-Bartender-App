eb261d336e0ce5a024e8ebcd236c6f63effb5a73
Bennett Hunter <jon.hunter.18@cnu.edu>
2021-11-11 15:50:37 -0500

    added order activity. did not implement

---
+++ app/src/main/java/com/example/autobartender/OrderInfo.java
@@ -0,0 +1,14 @@
+package com.example.autobartender;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OrderInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_info);
+    }
+}
